44aedd64031b7a6085198c1bd4a8140e5444a764
owain davies <ojdavies@hotmail.co.uk>
2024-09-10 00:11:59 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace SpaceInvaders_WPF
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		int refreshRate = 20;
+		DispatcherTimer gameTimer = new DispatcherTimer();
+
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void KeyDownEvent(object sender, KeyEventArgs e)
+		{
+
+        }
+
+		private void KeyUpEvent(object sender, KeyEventArgs e)
+		{
+
+		}
+	}
+}
