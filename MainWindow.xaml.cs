0d8d7987454512a399e62fe061c0c726cce177d7
owain davies <ojdavies@hotmail.co.uk>
2024-09-10 01:05:32 +0100

    Simple Left And Right Conteroles

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,18 +20,87 @@ namespace SpaceInvaders_WPF
 		int refreshRate = 20;
 		DispatcherTimer gameTimer = new DispatcherTimer();
 
+		bool leftDown, rightDown, spaceDown;
+
+		int playerSpeed = 10;
+		int playerMomentum = 0;
+		
 		public MainWindow()
 		{
 			InitializeComponent();
+
+			diplay.Focus();
+
+			gameTimer.Tick += GameLoop;
+			gameTimer.Interval = TimeSpan.FromMilliseconds(refreshRate);
+
+			StartGame();
+			
+
+		}
+
+		private void StartGame()
+		{
+			leftDown = false;
+			rightDown = false;
+			spaceDown = false;
+
+			gameTimer.Start();
+		}
+
+		private void GameLoop(object sender, EventArgs e)
+		{
+			HandelInputs();
+
+		}
+
+		private void HandelInputs()
+		{
+			if (leftDown ) { HandelLeftInput();  }
+			if (rightDown) { HandelRightInput(); }
+			if (spaceDown) { HandelSpaceInput(); }
+		}
+
+		private void HandelLeftInput() 
+		{ 
+		 if (!rightDown)
+			{
+				if (Canvas.GetLeft(player) - playerSpeed < 10) { Canvas.SetLeft(player, 10); }
+				else { Canvas.SetLeft(player, Canvas.GetLeft(player) - playerSpeed); }
+			}
+		
+		}
+
+		private void HandelRightInput() 
+		{
+
+			if (!leftDown)
+			{
+				if (Canvas.GetLeft(player) + playerSpeed > 800 - player.Width - 30)
+				{ Canvas.SetLeft(player, 800 - player.Width - 30); }
+				else { Canvas.SetLeft(player, Canvas.GetLeft(player) + playerSpeed); }
+			}
+		}
+
+		private void HandelSpaceInput() 
+		{
+		
+		
 		}
 
 		private void KeyDownEvent(object sender, KeyEventArgs e)
 		{
-
-        }
+			if (e.Key == Key.Left)  { leftDown  = true; }
+			if (e.Key == Key.Right) { rightDown = true; }
+			if (e.Key == Key.Space) { spaceDown = true; }
+		}
 
 		private void KeyUpEvent(object sender, KeyEventArgs e)
 		{
+
+			if (e.Key == Key.Left)  { leftDown  = false; }
+			if (e.Key == Key.Right) { rightDown = false; }
+			if (e.Key == Key.Space) { spaceDown = false; }
 
 		}
 	}
